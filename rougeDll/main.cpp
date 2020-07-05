f8b1ffe3b4e748a8207a9e0f7af85fbbebf90e8c
Sammy Hajhmaid <pokeso123@hotmail.com>
2020-07-04 21:40:59 -0700

    automatically enumerate the addresses of selected functions from lua51.dll

--- rougeDll/main.cpp
+++ rougeDll/main.cpp
@@ -1,30 +1,69 @@
 #include <windows.h>
 #include <cstring>
+#include <future>
+#include <unordered_map>
+#include <sstream>
 #include "detours.h"
 
-#pragma comment (lib, "User32.lib")
-#pragma comment (lib, "detours.lib")
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "detours.lib")
 
-typedef int (*_luaopen_jit)(void **state);
-_luaopen_jit luaopen_jit;
+typedef int (*luaopen_jit_t)(void **state);
+luaopen_jit_t luaopen_jit = NULL;
 
+typedef std::unordered_map<std::string, DWORD *> exportMap_t;
+exportMap_t exportsToResolve({
+    {"luaopen_jit", (DWORD *)&luaopen_jit},
+});
+size_t exportsResolved = 0;
 
+std::promise<void> fetchedExports;
 
-BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID p) {
-    switch (dwReason) {
-        case DLL_PROCESS_ATTACH:
-        HINSTANCE hInst;
-        for (hInst = NULL; (hInst = DetourEnumerateModules(hInst)) != NULL;) {
-            WCHAR dllPath[MAX_PATH];
-            GetModuleFileNameW(hInst, dllPath, sizeof(dllPath));
-            WCHAR *dllName;
-            if (!(dllName = wcsrchr(dllPath, '\\'))) {
-                continue;
-            }
-            if (!wcscmp(dllName + 1, L"lua51.dll")) {
-                break;
-            }
+BOOL ExportCallback(PVOID programContext, ULONG ordinal, LPCSTR name, PVOID addr)
+{
+    exportMap_t::const_iterator unresolvedExport = exportsToResolve.find(std::string(name));
+    if (unresolvedExport != exportsToResolve.end())
+    {
+        *(unresolvedExport->second) = (DWORD)addr;
+        if (++exportsResolved == exportsToResolve.size())
+        {
+            fetchedExports.set_value();
+            return false;
         }
+    }
+    return true;
+}
+
+void EnumerateExports()
+{
+    HINSTANCE hInst;
+    for (hInst = NULL; (hInst = DetourEnumerateModules(hInst)) != NULL;)
+    {
+        WCHAR dllPath[MAX_PATH];
+        GetModuleFileNameW(hInst, dllPath, sizeof(dllPath));
+        WCHAR *dllName;
+        if (!(dllName = wcsrchr(dllPath, '\\')))
+        {
+            continue;
+        }
+        if (!wcscmp(dllName + 1, L"lua51.dll"))
+        {
+            break;
+        }
+    }
+    std::future<void> afterExports = fetchedExports.get_future();
+    DetourEnumerateExports(hInst, NULL, (PF_DETOUR_ENUMERATE_EXPORT_CALLBACK)ExportCallback);
+    // wait for exports to finish
+    afterExports.get();
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID p)
+{
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // load all the addresses of the functions we will need
+        EnumerateExports();
         break;
     }
     return true;
